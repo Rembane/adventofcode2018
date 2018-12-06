50a6b0085b5916a2663b6ad4c9a5da1cbe902932
Andreas Ekeroot <andeke@gmail.com>
2018-12-06 15:09:21 +0100

    Solved day 3 in a very Pythonesque manner.

--- 3/Day3.hs
+++ 3/Day3.hs
@@ -1,65 +1,64 @@
+{-# LANGUAGE TupleSections #-}
 module Day3 where
 
 import           Control.Arrow                            ( first
                                                           , second
                                                           )
 import           Data.Function                            ( on )
-import           Data.Maybe                               ( catMaybes )
+import           Data.Ix                                  ( range )
+import           Data.List                                ( concat
+                                                          , concatMap
+                                                          , lines
+                                                          , group
+                                                          , groupBy
+                                                          , maximumBy
+                                                          , sort
+                                                          , sortOn
+                                                          )
+import           Data.Maybe                               ( mapMaybe )
+import qualified Data.Map.Strict               as M
+import           Data.Map.Strict                          ( Map )
 import           Data.Ord                                 ( comparing )
-import           Data.List
+import           Data.Tuple                               ( swap )
 
--- (x, y)
-newtype Point = Point { unPoint :: (Int, Int) } deriving (Eq)
-
-instance Ord Point where
-  compare p1 p2 = comparing (fst . unPoint) p1 p2
-               <> comparing (snd . unPoint) p1 p2
-
-instance Show Point where
-  show = show . unPoint
-
-data Rectangle = Rectangle
-  { topleft     :: Point
-  , bottomright :: Point
-  } deriving (Eq, Show)
-
-instance Ord Rectangle where
-  compare = comparing topleft
-
-inRectangle :: Point -> Rectangle -> Bool
-inRectangle p r = p >= topleft r && p <= bottomright r
-
--- If there is an overlap, we get back a new Rectangle with the overlap.
--- This function assumes that rectangle one is less than rectangle two.
-overlap :: Rectangle -> Rectangle -> Maybe Rectangle
-overlap r1 r2
-  | inRectangle (topleft r2) r1 || inRectangle (bottomright r1) r2
-  = pure $ Rectangle (Point (topX, min bottomY topY))
-                     (Point (bottomX, max topY bottomY))
-  | otherwise
-  = Nothing
- where
-  topY    = on max (snd . unPoint . topleft) r1 r2
-  topX    = on max (fst . unPoint . topleft) r1 r2
-  bottomY = on min (snd . unPoint . bottomright) r1 r2
-  bottomX = on min (fst . unPoint . bottomright) r1 r2
-
-area :: Rectangle -> Int
-area r = w * h
- where
-  h = snd (unPoint (bottomright r)) - snd (unPoint (topleft r))
-  w = fst (unPoint (bottomright r)) - fst (unPoint (topleft r))
-
-parseRectangle :: String -> Rectangle
+-- ... -> [((Claim id, (x, y))]
+parseRectangle :: String -> [(Int, (Int, Int))]
 parseRectangle s =
-  let (lft, rs1) = (first read . break (== ',') . tail . dropWhile (/= '@')) s
-      (tp , rs2) = (first read . break (== ':') . tail) rs1
-      (wd , rs3) = (first read . break (== 'x') . drop 2) rs2
-      ht         = (read . tail) rs3
-  in  Rectangle (Point (lft, tp)) (Point (lft + wd, tp + ht))
+  let (cid, rs0) = (first read . break (== ' ') . tail) s
+      (lft, rs1) =
+        (first read . break (== ',') . tail . dropWhile (/= '@')) rs0
+      (tp, rs2) = (first read . break (== ':') . tail) rs1
+      (wd, rs3) = (first read . break (== 'x') . drop 2) rs2
+      ht        = (read . tail) rs3
+      rs        = (,) <$> range (lft, lft + wd - 1) <*> range (tp, tp + ht - 1)
+  in  map ((,) cid) rs
+
+lengthOne :: [a] -> Bool
+lengthOne [_] = True
+lengthOne _   = False
 
 main :: IO ()
 main = do
-  xs <- sort . map parseRectangle . lines <$> readFile "input.txt"
-  print $ sum $ map area $ catMaybes $ zipWith overlap xs (tail xs)
-  -- print xs
+  cs <- concatMap parseRectangle . lines <$> readFile "input.txt"
+  putStr "Part 1: "
+  print
+    $ length
+    . filter (> 1)
+    . M.elems
+    . M.fromListWith (+)
+    . map ((, 1) . snd)
+    $ cs
+
+  putStr "Part 2: "
+  print
+    $ let coords = (M.fromListWith (++) . map (second pure . swap)) cs
+          m      = (M.fromListWith (++) . map (second pure)) cs
+      in  head
+          . mapMaybe
+              (\(cid, cs) ->
+                case all lengthOne <$> mapM (`M.lookup` coords) cs of
+                  Just True -> Just cid
+                  _         -> Nothing
+              )
+          $ M.assocs m
+
